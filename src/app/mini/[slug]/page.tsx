d65495dc93a126629cb93a8d6b1c224f7ec6b181
Murodjon Azamov <programmerislamic@gmail.com>
2025-04-02 17:12:19 +0500

    inital setup

--- src/app/mini/[slug]/page.tsx
+++ src/app/mini/[slug]/page.tsx
@@ -19,8 +19,8 @@ export default async function MiniAppPage({ params }: any) {
   return (
     <main className="min-h-screen flex items-center justify-center bg-white text-black">
       <div className="text-center space-y-2">
-        <h1 className="text-2xl font-bold">👋 Hello from Mini App!</h1>
         <p className="text-lg">This is for <strong>{app.name}</strong></p>
+        <h1 className="text-2xl font-bold">👋 Hello from Mini App!</h1>
         <p className="text-sm text-gray-500">Slug: <code>{app.slug}</code></p>
       </div>
     </main>
