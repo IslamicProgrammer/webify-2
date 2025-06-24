0647b05cf6fab61fbbb6d0989067f7b189ab4a6b
Murodjon Azamov <programmerislamic@gmail.com>
2025-06-24 14:07:04 +0500

    fix: fixed destination-form ui

--- src/app/dashboard/apps/[id]/page.tsx
+++ src/app/dashboard/apps/[id]/page.tsx
@@ -12,28 +12,6 @@ export default async function BotDetailPage({ params }: any) {
 
   await api.apps.getById.prefetch({ id: params.id });
 
-  const response = await fetch(
-    'http://localhost:3001/api/trpc/hello.hello', // ✅ call base `/api/trpc`
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify([
-        {
-          id: 1,
-          jsonrpc: '2.0',
-          method: 'query',
-          params: {
-            input: { id: params.id }
-          }
-        }
-      ])
-    }
-  );
-
-  console.log('Response from server:', response);
-
   return (
     <HydrateClient>
       <BotDetailContent id={params.id} />
