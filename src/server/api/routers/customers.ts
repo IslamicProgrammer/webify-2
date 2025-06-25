9265f8567dcdb93b2dbd7647dbc9c1e5e945fb34
Murodjon Azamov <programmerislamic@gmail.com>
2025-06-25 12:52:32 +0500

    feat: implemented changes

--- src/server/api/routers/customers.ts
+++ src/server/api/routers/customers.ts
@@ -28,7 +28,7 @@ const updateCustomerSchema = z.object({
 });
 
 const createCustomerSchema = z.object({
-  chatId: z.string().cuid().optional(),
+  chatId: z.string().optional(),
   appId: z.string().cuid(),
   firstName: z.string().min(1, 'First name is required').max(50, 'First name must be less than 50 characters'),
   lastName: z.string().min(1, 'Last name is required').max(50, 'Last name must be less than 50 characters'),
